47fd2c2989583b6ae93ffdc50b6955e5ffe71ef3
Olympia Thornton <olympiat@azerox.org>
2022-06-13 20:36:24 -0500

    inverse matrix and single line input

--- app.py
+++ app.py
@@ -18,24 +18,52 @@ def get_input() -> Matrix:
         # get the input
         temp = input()
         if temp:
+            if ',' in temp:
+                rows = temp.split(',')
+                for row in rows:
+                    matrix = matrix.row_insert(counter, Matrix(
+                        [[int(i) for i in row.split(' ') if i]]))
+                    counter += 1
             # add the row to the matrix if the row isn't empty
-            matrix = matrix.row_insert(counter, Matrix(
-                [[int(i) for i in temp.split(' ')]]))
-            counter += 1
+            else:
+                matrix = matrix.row_insert(counter, Matrix(
+                    [[int(i) for i in temp.split(' ') if i]]))
+                counter += 1
     return matrix
 
 
 def row_echelon(matrix: Matrix) -> Matrix:
     """Calculate and return the reduced row echelon form"""
-    return matrix.rref()
+    try:
+        temp_rref = matrix.rref()
+        pprint(temp_rref)
+        input()
+        return temp_rref
+    except:
+        print("Error calculating Reduced Row Echelone Form")
+        input()
 
 
 def determin(matrix: Matrix):
     """Calculate and return the determinent of a matrix"""
     try:
-        return matrix.det()
+        temp_det = matrix.det()
+        print(f'Determinent of matrix is: {temp_det}')
+        input()
+        return temp_det
     except sympy.matrices.common.NonSquareMatrixError:
         print("Matrix is not a Square Matrix")
+        input()
+        return 0
+
+
+def inverse(matrix: Matrix):
+    """Return the inverse of a matrix"""
+    try:
+        return matrix**-1
+    except sympy.matrices.common.NonSquareMatrixError:
+        print('Matrix is not invertible')
+        return 0
 
 
 def main_menu():
@@ -43,9 +71,10 @@ def main_menu():
     os.system('cls||clear')
     matrix = get_input()
     options = ['Reduced Row Echelon Form',
-               'Determinent', 'Change Matrix', 'Quit']
+               'Determinent', 'Invert', 'Change Matrix', 'Quit']
     rref = None
     det = None
+    inverted = None
 
     while True:
         # Clear the screen
@@ -74,31 +103,37 @@ def main_menu():
             os.system('cls||clear')
             if not rref:
                 rref = row_echelon(matrix)
-                pprint(rref)
-                input()
             else:
                 pprint(rref)
                 input()
+
         elif choice == 'Determinent':
             # Calculate the determinent if not already saved
             os.system('cls||clear')
-            if not det:
+            if det == None:
                 det = determin(matrix)
-                if not det:
-                    input()
-                    continue
-                pprint(matrix)
-                print(det)
-                input()
             else:
-                pprint(matrix)
-                print(det)
-                input()
+                print(f'Determinent of matrix is: {det}')
+
+        elif choice == 'Invert':
+            os.system('cls||clear')
+            if det != 0 and inverted != 0:
+                if inverted:
+                    pprint(inverted)
+                else:
+                    inverted = inverse(matrix)
+                    if inverted:
+                        pprint(inverted)
+            else:
+                print('The matrix is not inversible')
+            input()
 
         elif choice == 'Change Matrix':
             # Change the matrix into a new one
             os.system('cls||clear')
             matrix = get_input()
+            det = None
+            rref = None
         elif choice == 'Quit':
             break
 
